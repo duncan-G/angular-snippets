94611d8028bbebd49b749a38e1230338c78a4e21
duncan-G <30706687+duncan-G@users.noreply.github.com>
2018-04-17 01:11:54 -0700

    first commit

---
+++ tables/mat-table/myproducts.component.ts
@@ -0,0 +1,144 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { MyProductsService } from './products.service';
+import { DataSource } from '@angular/cdk/collections';
+import { Observable } from 'rxjs/Observable';
+import { MatPaginator, PageEvent } from '@angular/material';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import 'rxjs/add/operator/startWith';
+import 'rxjs/add/observable/merge';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/observable/fromEvent';
+import { Router } from '@angular/router';
+
+
+@Component({
+    selector   : 'my-products',
+    templateUrl: './myproducts.component.html',
+    styleUrls  : ['./myproducts.component.scss'],
+})
+export class MyProductsComponent implements OnInit
+{
+    pageEvent: PageEvent;
+    currentPageIndex: number = 0;
+    dataSource: FilesDataSource | null;
+    public showLoadingBar: boolean;
+    displayedColumns = ['main_image', 'title', 'category', 'price', 'quantity', 'state','has_variations'];
+
+    @ViewChild(MatPaginator) paginator: MatPaginator;
+    @ViewChild('scrollTo') elRef: ElementRef;
+    
+    constructor(
+        private productsService: MyProductsService,
+    )
+    {
+    }
+
+    ngOnInit()
+    {
+        this.dataSource = new FilesDataSource(this.productsService, this.paginator);
+    }
+
+    loadPageData(event) {
+        this.showLoadingBar = true;
+        if (event.pageIndex > this.currentPageIndex){
+            this.productsService.pageUrl = this.productsService.next;
+        } else if (event.pageIndex < this.currentPageIndex){
+            this.productsService.pageUrl = this.productsService.previous;
+        };
+        
+        this.productsService.getProducts()
+            .then(()=> {
+                // Scroll to the top of the page when pressing next or previous
+                this.elRef.nativeElement.scrollIntoView();
+                this.showLoadingBar = null;
+            });
+        this.currentPageIndex = event.pageIndex;
+    }
+}
+
+export class FilesDataSource extends DataSource<any>
+{
+    _filterChange = new BehaviorSubject('');
+    _filteredDataChange = new BehaviorSubject('');
+
+    get filteredData(): any
+    {
+        return this._filteredDataChange.value;
+    }
+
+    set filteredData(value: any)
+    {
+        this._filteredDataChange.next(value);
+    }
+
+    get filter(): string
+    {
+        return this._filterChange.value;
+    }
+
+    set filter(filter: string)
+    {
+        this._filterChange.next(filter);
+    }
+
+    constructor(
+        private productsService: MyProductsService,
+        private _paginator: MatPaginator,
+    )
+    {
+        super();
+        this.filteredData = this.productsService.products;
+    }
+
+    /** Connect function called by the table to retrieve one stream containing the data to render. */
+    connect(): Observable<any[]>
+    {
+        const displayDataChanges = [
+            this.productsService.onProductsChanged,
+            this._paginator.page,
+            this._filterChange,
+        ];
+
+        return Observable.merge(...displayDataChanges).map(() => {
+            let data = this.productsService.products.slice();
+
+            data = this.filterData(data);
+
+            this.filteredData = [...data];
+
+            // Grab the page's slice of data.
+            const startIndex = this._paginator.pageIndex * this._paginator.pageSize;
+            return data.splice(0,50);
+            //return data.splice(startIndex, this._paginator.pageSize);
+        });
+    }
+
+    filterData(data)
+    {
+        if ( !this.filter )
+        {
+            return data;
+        }
+        return this.filterArrayByString(data, this.filter);
+    }
+    
+    public static filterArrayByString(mainArr, searchText)
+    {
+        if ( searchText === '' )
+        {
+            return mainArr;
+        }
+
+        searchText = searchText.toLowerCase();
+
+        return mainArr.filter(itemObj => {
+            return this.searchInObj(itemObj, searchText);
+        });
+    }
+
+    disconnect()
+    {
+    }
+}
