1808302207e555eae6848a9cd17ed4d769f05429
duncan-G <30706687+duncan-G@users.noreply.github.com>
2018-04-17 00:36:08 -0700

    Update models.ts

--- authguards/pendingchanges/models.ts
+++ authguards/pendingchanges/models.ts
@@ -1,5 +1,6 @@
 export class DataModel
 {
+    data_id     : number;
     field_one   : string;
     field_two   : string;
     field_three : string;
@@ -7,6 +8,7 @@ export class DataModel
     constructor(data?)
     {
       data = data || {};
+      this.data_id = data.data_id || 0;   //Optionally generate a new id
       this.field_one = data.field_one || '';
       this.field_two = data.field_two || '';
       this.field_three = data.field_three || '';
