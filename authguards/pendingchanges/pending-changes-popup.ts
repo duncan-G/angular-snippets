a40773937ae909f271d5c7aed0ca78e528f42f7f
duncan-G <30706687+duncan-G@users.noreply.github.com>
2018-04-17 00:04:13 -0700

    Add files via upload

---
+++ authguards/pendingchanges/pending-changes-popup.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+/**
+ * @Pending Changes Popup
+ */
+
+@Component({
+    selector   : 'pending-changes-popup',
+    templateUrl: 'pending-changes-popup.html'
+})
+export class PendingChangesPopupDialog
+{
+
+}
