9e693e3dd7fea5de670d3ae547f9ccef97ddeecb
duncan-G <30706687+duncan-G@users.noreply.github.com>
2018-04-17 00:02:49 -0700

    first commit

---
+++ authguards/pendingchanges/pending-changes.guard.ts
@@ -0,0 +1,16 @@
+import { CanDeactivate } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { Injectable } from '@angular/core';
+
+
+export interface CanComponentDeactivate {
+    canDeactivate: () => Observable<boolean> | Promise<boolean> | boolean;
+  }
+  
+@Injectable()
+export class PendingChangesGuard implements  CanDeactivate<CanComponentDeactivate>{
+
+    canDeactivate(component: CanComponentDeactivate) {
+        return component.canDeactivate ? component.canDeactivate() : true;
+    }
+}
