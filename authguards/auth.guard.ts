a4f093f81fd8e173c534f7e28183ca8475ac18c9
duncan-G <30706687+duncan-G@users.noreply.github.com>
2018-04-17 00:00:29 -0700

    Update auth.guard.ts

--- authguards/auth.guard.ts
+++ authguards/auth.guard.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Router, CanActivate } from '@angular/router';
 
-import { AuthService } from '../content/authentication/authentication.service'
+import { AuthService } from '../authentication.service'
 
 
 @Injectable()
